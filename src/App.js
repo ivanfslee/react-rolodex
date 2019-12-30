75d1141ea78d3a44a772b3bd152ced69fa0e1409
ivanfslee <ivanfslee@gmail.com>
2019-12-29 19:56:45 -0800

    Added button to schange state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello bunghole'
+    };
+  }
+
+  clickHandler = () => {
+    this.setState({string: 'Hello moto'})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.string}</h1>
+        <button onClick={this.clickHandler}>Change Text</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
